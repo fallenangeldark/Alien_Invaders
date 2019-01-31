4ce7fe0d3d49c35874e7c657594c39198bf0fb8d
fallenangeldark <piedbox@gmail.com>
2019-01-31 20:10:48 +1000

    Add image folder with ship.png, add ship.py

--- alien_invaders.py
+++ alien_invaders.py
@@ -1,17 +1,21 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 def run_game():
-    # Инициализирует игру и создает объект экрана
+    # Инициализирует pygame, settings и объект экрана
     pygame.init()
-    screen = pygame.display.set_mode((1200, 700))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invaders")
     # Назначение цвета фона
-    bg_color = (230, 230, 230)
+    ship = Ship(screen)
     while True:
         # Отслеживание событий клавиатуры и мыши.
         for event in pygame.event.get():
-            screen.fill(bg_color)
+            screen.fill(ai_settings.bg_color)
+            ship.blitme()
             if event.type == pygame.QUIT:
                 sys.exit()
 
